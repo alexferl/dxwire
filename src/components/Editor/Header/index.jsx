afb809e3040d6adc584d5320f6c907fc9f7d8c49
alexferl <3533424+alexferl@users.noreply.github.com>
2026-02-09 20:19:56 -0500

    refactor: store everything under the same key (#1)
    Signed-off-by: alexferl <me@alexferl.com>

--- src/components/Editor/Header/index.jsx
+++ src/components/Editor/Header/index.jsx
@@ -1,5 +1,14 @@
 import { DX7Bank } from "midiwire"
 import { useEffect, useState } from "preact/hooks"
+import {
+  GearIcon,
+  HelpIcon,
+  LoadingSpinner,
+  ReceiveBankIcon,
+  SendBankIcon,
+  SendVoiceIcon,
+  SettingsIcon,
+} from "../../Icons/index.jsx"
 import { MenuButton, MenuItem } from "../../MenuButton/index.jsx"
 import { MIDIDeviceSelector } from "../../MIDIDeviceSelector/index.jsx"
 import { MIDIContextProvider, useMIDI } from "../context/MIDIContext.jsx"
@@ -11,119 +20,6 @@ import { HelpModal } from "./HelpModal.jsx"
 import { ImportMenu } from "./ImportMenu.jsx"
 import { SettingsModal } from "./SettingsModal.jsx"
 import "./style.css"
-
-const receiveBankIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="18"
-    viewBox="0 0 24 24"
-    width="18"
-    fill="currentColor"
-    role="img"
-    aria-label="Receive bank icon"
-  >
-    <title>Receive bank icon</title>
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z" />
-  </svg>
-)
-
-const sendVoiceIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="18"
-    viewBox="0 0 24 24"
-    width="18"
-    fill="currentColor"
-    role="img"
-    aria-label="Send voice icon"
-  >
-    <title>Send voice icon</title>
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M12 3v9.28c-.47-.17-.97-.28-1.5-.28C8.01 12 6 14.01 6 16.5S8.01 21 10.5 21c2.31 0 4.2-1.75 4.45-4H15V6h4V3h-7z" />
-  </svg>
-)
-
-const loadingSpinner = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="18"
-    viewBox="0 0 24 24"
-    width="18"
-    fill="currentColor"
-    role="img"
-    aria-label="Loading"
-    class="spinner"
-  >
-    <title>Loading</title>
-    <path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z" />
-  </svg>
-)
-
-const sendBankIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="18"
-    viewBox="0 0 24 24"
-    width="18"
-    fill="currentColor"
-    role="img"
-    aria-label="Send bank icon"
-  >
-    <title>Send bank icon</title>
-    <path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z" />
-  </svg>
-)
-
-/** @type {import("preact").VNode} */
-const helpIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="20"
-    viewBox="0 0 24 24"
-    width="20"
-    fill="currentColor"
-    role="img"
-    aria-label="Help icon"
-  >
-    <title>Help icon</title>
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z" />
-  </svg>
-)
-
-const settingsIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="20"
-    viewBox="0 0 24 24"
-    width="20"
-    fill="currentColor"
-    role="img"
-    aria-label="Settings icon"
-  >
-    <title>Settings icon</title>
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
-  </svg>
-)
-
-/** @type {import("preact").VNode} */
-const gearIcon = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    height="20"
-    viewBox="0 0 24 24"
-    width="20"
-    fill="currentColor"
-    role="img"
-    aria-label="Settings gear icon"
-  >
-    <title>Settings gear icon</title>
-    <path d="M0 0h24v24H0z" fill="none" />
-    <path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58a.49.49 0 0 0 .12-.61l-1.92-3.32a.488.488 0 0 0-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54a.484.484 0 0 0-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L3.16 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58a.49.49 0 0 0-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z" />
-  </svg>
-)
 
 /**
  * Bank management menu component.
@@ -156,7 +52,7 @@ function BankManageMenu() {
 
   return (
     <>
-      <MenuButton icon={settingsIcon} title="Manage Bank">
+      <MenuButton icon={<SettingsIcon size="lg" ariaLabel="Settings icon" />} title="Manage Bank">
         <MenuItem label="Rename Bank..." onClick={() => setShowRename(true)} />
         <MenuItem label="Delete Bank" onClick={handleDelete} disabled={!canDelete} />
         <MenuItem label="Separator" separator />
@@ -203,7 +99,7 @@ function VoiceManageMenu() {
 
   return (
     <>
-      <MenuButton icon={settingsIcon} title="Manage Voice">
+      <MenuButton icon={<SettingsIcon size="lg" ariaLabel="Settings icon" />} title="Manage Voice">
         <MenuItem label="Initialize Voice" onClick={handleInit} />
         <MenuItem label="Rename Voice..." onClick={() => setShowRename(true)} />
         <MenuItem label="Separator" separator />
@@ -394,7 +290,11 @@ function HeaderContent() {
             disabled={!hasInputDevice || requestingBank}
             title="Receive Bank from Device"
           >
-            {requestingBank ? loadingSpinner : receiveBankIcon}
+            {requestingBank ? (
+              <LoadingSpinner size="md" />
+            ) : (
+              <ReceiveBankIcon size="md" ariaLabel="Receive bank icon" />
+            )}
           </button>
           <button
             type="button"
@@ -403,7 +303,7 @@ function HeaderContent() {
             disabled={!hasOutputDevice || !bankLoaded || sendingBank}
             title="Send Bank to Device"
           >
-            {sendingBank ? loadingSpinner : sendBankIcon}
+            {sendingBank ? <LoadingSpinner size="md" /> : <SendBankIcon size="md" ariaLabel="Send bank icon" />}
           </button>
           <button
             type="button"
@@ -412,7 +312,7 @@ function HeaderContent() {
             disabled={!hasOutputDevice || sendingVoice}
             title="Send Voice to Device"
           >
-            {sendingVoice ? loadingSpinner : sendVoiceIcon}
+            {sendingVoice ? <LoadingSpinner size="md" /> : <SendVoiceIcon size="md" ariaLabel="Send voice icon" />}
           </button>
         </div>
         <div class="header-right">
@@ -424,10 +324,10 @@ function HeaderContent() {
             <ExportMenu />
           </div>
           <button type="button" class="settings-button" onClick={() => setShowSettings(true)} title="Settings">
-            {gearIcon}
+            <GearIcon size="lg" ariaLabel="Settings gear icon" />
           </button>
           <button type="button" class="help-button" onClick={() => setShowHelp(true)} title="Help">
-            {helpIcon}
+            <HelpIcon size="lg" ariaLabel="Help icon" />
           </button>
         </div>
       </div>
